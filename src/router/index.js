acc1448fcdc6383c61321cf01cf713b02c6b746e
Tuan <tuan.voanh@hdwebsoft.co>
2021-05-19 18:02:52 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import {createRouter} from 'vue-router'
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+})
+
+export default router
